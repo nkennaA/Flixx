1a2860d22661ff77747361105b22a45a2747955e
Nkenna Aniedobe <nkenna@fb.com>
2018-06-27 11:11:43 -0700

    Initial Commit

---
+++ Flixx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flixx
+//
+//  Created by Nkenna Aniedobe on 6/27/18.
+//  Copyright © 2018 Nkenna Aniedobe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flixx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flixx
+//
+//  Created by Nkenna Aniedobe on 6/27/18.
+//  Copyright © 2018 Nkenna Aniedobe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
