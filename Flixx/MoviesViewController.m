2f865e8f9653171fdd193c0658787b35dfd7c184
Nkenna Aniedobe <nkenna@fb.com>
2018-06-28 10:52:38 -0700

    Tables with movie titles

--- Flixx/MoviesViewController.m
+++ Flixx/MoviesViewController.m
@@ -8,15 +8,49 @@
 
 #import "MoviesViewController.h"
 
-@interface MoviesViewController ()
+@interface MoviesViewController ()<UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) NSArray *movies;
+@property (weak, nonatomic) IBOutlet UITableView *movieTableView;
 
 @end
 
 @implementation MoviesViewController
 
 - (void)viewDidLoad {
+    self.movieTableView.delegate = self;
+    self.movieTableView.dataSource =self;
     [super viewDidLoad];
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+        else {
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            NSLog(@"%@", dataDictionary);
+            self.movies = dataDictionary[@"results"];
+            for(NSDictionary *movie in self.movies){
+                NSLog(@"%@", movie[@"title"]);
+            }
+            [self.movieTableView reloadData];
+            // TODO: Get the array of movies
+            // TODO: Store the movies in a property to use elsewhere
+            // TODO: Reload your table view data
+        }
+    }];
+    [task resume];
     // Do any additional setup after loading the view.
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.movies.count;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    NSDictionary *movie = self.movies[indexPath.row];
+    cell.textLabel.text = movie[@"title"];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
