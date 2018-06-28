92037c621c1deee222af419d9f7adbf0362321ba
Nkenna Aniedobe <nkenna@fb.com>
2018-06-28 10:52:00 -0700

    movie titles in table

---
+++ Flixx/MoviesViewController.m
@@ -0,0 +1,37 @@
+//
+//  MoviesViewController.m
+//  Flixx
+//
+//  Created by Nkenna Aniedobe on 6/27/18.
+//  Copyright © 2018 Nkenna Aniedobe. All rights reserved.
+//
+
+#import "MoviesViewController.h"
+
+@interface MoviesViewController ()
+
+@end
+
+@implementation MoviesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
